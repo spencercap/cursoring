97a8e08178c713af272eeeb4126b639c29b1c4bb
spencercap <cappiello.spencer@gmail.com>
2020-12-06 17:44:23 +0000

    added firebase (still needs to be put in the backend)

--- src/content-scripts/content-script.ts
+++ src/content-scripts/content-script.ts
@@ -1,5 +1,7 @@
 console.log('Hello from the content-script');
 
+
+
 const el = document.createElement('BUTTON');
 el.style.position = 'absolute';
 el.style.transitionDuration = '200ms';
@@ -15,3 +17,35 @@ setInterval(() => {
 
 
 
+// FIREBASE
+import firebase from 'firebase/app';
+import 'firebase/auth';
+// import 'firebase/firestore';
+import 'firebase/database';
+
+// firebase config
+const config = {
+	apiKey: 'AIzaSyDgZLBk6ibee-z9Ii-lZHtf7TtCgyIw3VI',
+	authDomain: 'cursoring.firebaseapp.com',
+	databaseURL: 'https://cursoring-default-rtdb.firebaseio.com',
+	projectId: 'cursoring',
+	storageBucket: 'cursoring.appspot.com',
+	messagingSenderId: '659784909263',
+	appId: '1:659784909263:web:943bb8ba3b398e0b13d2ba',
+	measurementId: 'G-ZFDDERW432'
+};
+const firebaseApp = firebase.initializeApp(config); // credentials.config
+const fb = firebase;
+const auth = firebaseApp.auth();
+// const db = firebaseApp.firestore();
+const rtdb = firebaseApp.database();
+
+console.log('got', fb, auth, rtdb);
+
+
+auth.onAuthStateChanged(function (user) {
+	console.log('got a user', user);
+});
+
+
+auth.signInAnonymously();
